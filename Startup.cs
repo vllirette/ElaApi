6ccb4919711d8fdc2c6d368a89542c54a303b283
vincentLirette <vi.lirette@gmail.com>
2018-11-25 17:54:55 -0500

    Add Swagger

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 using ElaApi.Models;
 using ElaApi.Repositories;
 
@@ -37,6 +38,12 @@ namespace ElaApi
 
             services.AddTransient<IMemberContext, MemberContext>();
             services.AddTransient<IMemberRepository, MemberRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "ElaAPI", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +59,18 @@ namespace ElaApi
             }
 
             app.UseHttpsRedirection();
+            
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ElaAPI v1");
+                c.RoutePrefix = string.Empty;
+
+            });
             app.UseMvc();
         }
     }
